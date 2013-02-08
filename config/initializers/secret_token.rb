2c15ca7dc152d5b248cb30e7ba469d8be831dfb5
Adam Rensel <adamrensel@gmail.com>
2013-02-08 14:45:34 -0500

    Add initial files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JavascriptParsetree::Application.config.secret_token = '4e64c796cfe55708a12a99c61242a07286547fc1ffa5a3cb594dcbeb8b5e127dfb42c14dab6dd912fc9a2bb20b0c9c7fbd57fa94ce173a220fb060358e1ed1ab'
